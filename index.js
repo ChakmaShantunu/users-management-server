434f12078f932544243c3931145fa586270a108c
shantunu007 <chakmashantunu.web@gmail.com>
2026-07-24 11:51:04 +0600

    create users endpoint and enable CORS

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
 const express = require("express");
+const cors = require("cors")
 const app = express();
-const port = 3000
+const port = process.env.port || 3000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Hello world")
+})
+
+const users = [
+    {
+        id: 1,
+        name: "Rahim Uddin",
+        email: "rahim@example.com",
+        age: 28,
+        role: "Admin"
+    },
+    {
+        id: 2,
+        name: "Karim Ahmed",
+        email: "karim@example.com",
+        age: 24,
+        role: "User"
+    },
+    {
+        id: 3,
+        name: "Fatema Khatun",
+        email: "fatema@example.com",
+        age: 26,
+        role: "Moderator"
+    },
+    {
+        id: 4,
+        name: "Sakin Islam",
+        email: "sakin@example.com",
+        age: 30,
+        role: "User"
+    }
+];
+
+app.get("/users", (req, res) => {
+    res.send(users);
 })
 
 app.listen(port, () => {
